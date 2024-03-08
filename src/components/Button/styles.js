4fd3cdb6fc509fe93ba26b069b4647cac1569b0f
Fraanzinhaa <francielle.eal@gmail.com>
2024-03-08 09:35:22 -0300

    commit inicial

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #0a3d62;
+  background-color: #079992;
+  color: #FFFFF;
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+  border-radius: 4px;
+  font-family: 'Roboto', sans-serif;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`
+
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #82ccdd;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  
+
+  font-size: 24px;
+  font-family: 'Roboto', sans-serif;
+  
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #60a3bc;
+    border: 0;
+    border-radius: 6px;
+    border: 1px solid #0a3d62;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    padding: 0 10px;
+    color: #FFFFFF;
+  }
+`
